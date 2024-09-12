4e6bc838adc8e0d77ae7bf7e452a386862fb65f2
ADMIN <nththuong2505@gmail.com>
2024-09-13 02:07:05 +0700

    Nguyễn Thị Huyền Thương Msv: 22115053122347

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-   //comment
+
     }
 }
 
