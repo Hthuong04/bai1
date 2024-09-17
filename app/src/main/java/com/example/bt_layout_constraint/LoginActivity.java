762b4667f1b11d0cf5dea395c712ab3e825d4a92
ADMIN <nththuong2505@gmail.com>
2024-09-17 23:52:37 +0700

    Bt3

---
+++ app/src/main/java/com/example/bt_layout_constraint/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.example.bt_layout_constraint;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        EditText editTextInput;
+        EditText editTextInputPass;
+        Button buttonShow;
+        TextView textViewOutput;
+        TextView textViewOutput1;
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+        editTextInput = findViewById(R.id.inputUsername);
+        editTextInputPass = findViewById(R.id.inputPassword);
+        buttonShow = findViewById(R.id.btn_login);
+        textViewOutput = findViewById(R.id.textView7);
+        textViewOutput1 = findViewById(R.id.textView8);// Kiểm tra ID trong layout
+
+        buttonShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = editTextInput.getText().toString(); // Lấy nội dung từ EditText
+                textViewOutput.setText(inputText); // Hiển thị nội dung trong TextView
+                String inputText1 = editTextInputPass.getText().toString(); // Lấy nội dung từ EditText
+                textViewOutput1.setText(inputText1);
+            }
+        });
+        TextView btn = findViewById(R.id.textviewSignup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/bt_layout_constraint/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.example.bt_layout_constraint;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        ConstraintLayout btn = findViewById(R.id.alreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
